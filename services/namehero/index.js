e87e54acf4ca9d7b231645e644ac7aaca6a6e45d
Deniz Egemen Coşkun <denizegemencoskun@gmail.com>
2020-10-03 18:30:23 +0300

    namehero/index.js :: Send the user name by querying the database.

--- services/namehero/index.js
+++ services/namehero/index.js
@@ -1,6 +1,7 @@
 'use strict';
 
 const express = require('express');
+const database = require('./db.js');
 
 // Constants
 const PORT = 3000;
@@ -9,8 +10,18 @@ const HOST = '0.0.0.0';
 // App
 const app = express();
 app.get('/', (req, res) => {
-  // Send a random name
-  res.send(`Deniz`);
+  // Send a random name from database
+  database.connection.query('SELECT * FROM names;', function (err, result) {
+    if (err) throw err;
+
+    var names = [];
+    for (let i = 0; i < result.length; i++) {
+      const element = result[i];
+      names.push(element.name);
+    }
+
+    res.send(names[Math.floor(Math.random() * names.length)]);
+  });
 });
 
 app.listen(PORT, HOST);
